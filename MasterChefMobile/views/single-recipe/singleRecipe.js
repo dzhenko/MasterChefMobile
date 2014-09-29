e0511b71e57a4871a66983d093393da704b65045
Dzhenko Penev <dzhenko@gmail.com>
2014-09-29 14:00:41 -0500

    Changes made by Dzhenko Penev (dzhenko@gmail.com).

--- MasterChefMobile/views/single-recipe/singleRecipe.js
+++ MasterChefMobile/views/single-recipe/singleRecipe.js
@@ -9,7 +9,7 @@ app.singleRecipe = app.singleRecipe || {};
             var recipeId = e.view.params.id;
             
             app.requester.recipe.byId(recipeId).then(function(data) {
-                console.log(data);
+                $('#single-recipe-image-holder').css('background-image','url(' + data.Image + ')');
                 var vm = new kendo.observable({
                     Image: data.Image,
                     Name: data.Name,
@@ -17,7 +17,19 @@ app.singleRecipe = app.singleRecipe || {};
                     Description: data.Description,
                     PreparationSteps: data.PreparationSteps,
                     Products: data.Products,
-                    Owner: data.Owner
+                    Owner: data.Owner,
+                    likeText: 'Like',
+                    onLikeClick : function() {
+                        app.requester.actions.like(data.Id).then(function() {
+                            console.log('liked');
+                            this.set('likeText', this.get('likeText') === "Like" ? "Unlike" : "Like");
+                        });
+                    },
+                    onCommentClick : function() {
+                        app.requester.actions.comment(data.Id, $('#commentText').val());
+                        $('#commentText').val('');
+                        location.reload();
+                    },
                 });
                 
                 app.singleRecipe.model = vm;

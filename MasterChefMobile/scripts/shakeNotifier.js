acf227795226f6e0cee9382d35b17eb0e9bbcc9e
Dzhenko Penev <dzhenko@gmail.com>
2014-09-29 12:45:38 -0500

    Changes made by Dzhenko Penev (dzhenko@gmail.com).

--- MasterChefMobile/scripts/shakeNotifier.js
+++ MasterChefMobile/scripts/shakeNotifier.js
@@ -37,7 +37,7 @@ app.shakeNotifier = (function () {
             accelerationChange.y = Math.abs(previousAcceleration.y, acceleration.y);
             accelerationChange.z = Math.abs(previousAcceleration.z, acceleration.z);
         }
-        if (accelerationChange.x  > 8) {
+        if (accelerationChange.x  > 12) {
             // Shake detected
             if (typeof shakeCallBack === "function") {
                 shakeCallBack();
